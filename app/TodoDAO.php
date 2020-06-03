728b1519723a5fa24f53799155a7cabeec698067
Frederic GAURAT <fgaurat@eolem.com>
2020-06-03 11:36:13 +0200

    add dao

--- app/TodoDAO.php
+++ app/TodoDAO.php
@@ -4,4 +4,27 @@ namespace App\Data;
 
 class TodoDAO{
     
+
+    private $dbh;
+
+    public function __construct()
+    {
+        $this->dbh = new \PDO('mysql:host=localhost;dbname=bdd_todos', 'user_todo', '12345');
+    }
+
+
+    public function findAll(){
+        $arr_todos = [];
+        $sth = $this->dbh->prepare('SELECT * from todos');
+        $sth->execute();
+        
+
+        foreach($sth->fetchAll(\PDO::FETCH_ASSOC) as $todo){
+            $t = new Todo($todo['id'],$todo['title'],$todo['done'],$todo['due_date']);
+            $arr_todos[] = $t;
+        }
+        return $arr_todos;
+    
+    }
+
 }
--- test_todo.php
+++ test_todo.php
@@ -1,11 +1,17 @@
+<pre>
 <?php
 include('./vendor/autoload.php');
 
 
 use App\Data\Todo;
-
-
+use App\Data\TodoDAO;
 
 $t = new Todo(1,'une todo',true,12345);
-
 echo $t->title;
+
+$dao = new TodoDAO();
+$arr_todos = $dao->findAll();
+print_r($arr_todos[0]);
+echo count($arr_todos);
+?>
+</pre>
